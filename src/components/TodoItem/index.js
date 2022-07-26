04c9abafaa85f6a224420b2c23f1e65c68e37f2e
DilipKumar <dilipkumar.vaddepally141@gmail.com>
2022-07-26 12:02:40 +0000

    Simple Todo

---
+++ src/components/TodoItem/index.js
@@ -0,0 +1,20 @@
+import './index.css'
+
+const TodoItem = props => {
+  const {todoDetails, deleteItem} = props
+
+  const {title, id} = todoDetails
+
+  const onDelete = () => deleteItem(id)
+
+  return (
+    <li className="todoItems-container">
+      <p className="todo-Item-Name">{title}</p>
+      <button className="delete-btn" type="button" onClick={onDelete}>
+        Delete
+      </button>
+    </li>
+  )
+}
+
+export default TodoItem
